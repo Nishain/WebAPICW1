d93dd1c0a46870d4a5f0041a5320c8e62f76d480
Isuru2175 <isuru2175@gmail.com>
2021-05-10 23:01:02 +0530

    input functinality implementtation

--- front-end/src/pages/Admin/CourierService/CourierService.jsx
+++ front-end/src/pages/Admin/CourierService/CourierService.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import React, { useEffect, useState, useCallback } from "react";
+import { Input } from "antd";
+import CustomTextBox from "../Shared/commonTextBox/CustomTextBox";
+import CourierServiceData from "../Data/districts.json";
 export default function CourierService() {
-    return (
-        <div className="container">
-            <h1>Courier Service</h1>
+  const [values, setValues] = useState(CourierServiceData);
+
+  const onChangeInputHandler = (id, price) => {
+    values.map((x, i) => {
+      if (x.id == id) {
+        x.value = price;
+      }
+
+      setValues([...values]);
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>Courier Service</h1>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <div className="form-group">
+            <label>Courier Service Name</label>
+            <Input placeholder="Basic usage" />
+          </div>
         </div>
-    )
+        <div className="col-md-4"></div>
+      </div>
+      <div className="row">
+        {values.length > 0 &&
+          values.map((data) => (
+            <div className="col-md-4">
+              <CustomTextBox obj={data} func={onChangeInputHandler} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
