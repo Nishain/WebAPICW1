2a55172b485a90ec11434001056b148aeb73048c
Isuru2175 <isuru2175@gmail.com>
2021-05-14 20:55:03 +0530

    add Chart

--- front-end/src/pages/Admin/Dashboard/Dashboard.jsx
+++ front-end/src/pages/Admin/Dashboard/Dashboard.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import ReactEcharts from "echarts-for-react";
 export default function Dashboard() {
+    const option = {
+        xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+        type: 'value'
+        },
+        series: [{
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+        }]
+        };
+
+const option2 = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+        source: [
+            ['product', '2012', '2013', '2014', '2015'],
+            ['Matcha Latte', 41.1, 30.4, 65.1, 53.3],
+            ['Milk Tea', 86.5, 92.1, 85.7, 83.1],
+            ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4]
+        ]
+    },
+    xAxis: [
+        {type: 'category', gridIndex: 0},
+        {type: 'category', gridIndex: 1}
+    ],
+    yAxis: [
+        {gridIndex: 0},
+        {gridIndex: 1}
+    ],
+    grid: [
+        {bottom: '55%'},
+        {top: '55%'}
+    ],
+    series: [
+        // These series are in the first grid.
+        {type: 'bar', seriesLayoutBy: 'row'},
+        {type: 'bar', seriesLayoutBy: 'row'},
+        {type: 'bar', seriesLayoutBy: 'row'},
+        // These series are in the second grid.
+        {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+        {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+        {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+        {type: 'bar', xAxisIndex: 1, yAxisIndex: 1}
+    ]
+};
     return (
         <div className="container">
             <h1>Dashbord</h1>
+            <ReactEcharts option={option} />
+            <ReactEcharts option={option2} />
         </div>
     )
 }
